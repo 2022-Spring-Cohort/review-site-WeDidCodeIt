e4b1cf230cf77c486791bbc4dc517c7ef8765e6c
HungryManSteve <mliddy09@gmail.com>
2022-02-17 14:03:27 -0500

    updated populator

--- src/main/java/org/wedidcodeit/reviews/Populator.java
+++ src/main/java/org/wedidcodeit/reviews/Populator.java
@@ -16,7 +16,16 @@ public class Populator implements CommandLineRunner {
         Language language = new Language(42, "Java","name of the inventor",true ,false,false,true);
         languageRepo.save(language);
 
-        languageRepo.save(new Language(24, "Python","name of the inventor",true ,false,false,true));
+//        languageRepo.save(new Language(24, "Python","name of the inventor",true ,false,false,true));
+        languageRepo.save(new Language(34, "Kotlin", "JetBrains", false, true,true, true));
+        languageRepo.save(new Language(33, "Python", "Guido van Rossum", true, false, true, true));
+        languageRepo.save(new Language(22, "Swift", "Ted Kremenek", false, true, true, true));
+        languageRepo.save(new Language(64, "C#", "Microsoft", false, true, true, true));
+        languageRepo.save(new Language(76, "Go", "Robert Pike", false, true, true, true));
+        languageRepo.save(new Language(35, "PHP", "Ramsmus Lerdorf", false, true, true, true));
+        languageRepo.save(new Language(94, "MatLab", "Cleve Moler", true, false, true, true));
+        languageRepo.save(new Language(83, "R","Robert Gentleman", true, false, false, true));
+        languageRepo.save(new Language(47, "HTML/CSS", "World Wide Web Consortium", true, false, true, false));
 
     }
 
